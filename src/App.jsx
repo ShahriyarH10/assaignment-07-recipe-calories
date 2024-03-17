f2ab5593b74ac12ba7804d0bee3df1677d76dc8d
ShahriyarH10 <md.shahriyarhasan2002me@gmail.com>
2024-03-17 11:07:54 +0600

    setup tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Vite + React</h1>
+      
+      <h1 className='text-3xl font-bold'>Vite + React</h1>
       
     </>
   )

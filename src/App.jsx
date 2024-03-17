e6f90bf3847687ec43bd85285d288450165c3dd9
ShahriyarH10 <md.shahriyarhasan2002me@gmail.com>
2024-03-17 11:47:34 +0600

    banner section added

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,16 @@ function App() {
         </div>
       </div>
       
-      <div className='my-10 container mx-auto'>
-        <h1 className='text-5xl font-bold'>Discover an exceptional <br /> cooking class tailored for you!</h1>
+      <div className='bg-img my-10 container mx-auto rounded-3xl flex flex-col justify-center items-center text-center'>
+        <div className='text-white'>
+          <h1 className='text-5xl font-bold'>Discover an exceptional <br /> cooking class tailored for you!</h1>
+          <p>Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding <br /> problems to become an exceptionally well world-class Programmer.</p>
+        </div>
+      
+        <div className='flex gap-5 font-bold'>
+          <button className='bg-green-600 rounded-3xl p-3 border-solid border-2 border-green-500'>Explore Now</button>
+          <button className='bg-none text-white rounded-3xl p-3 border-solid border-2 border-white '>Our Feedback</button>
+        </div>
       </div>
     </>
   )
